0004e8fbdbec5e39b9476af4082825ed5719af06
Sonu Chhetri <sonuchh21@gmail.com>
2024-12-14 22:17:40 +0530

    Delete Python/Assignment-2(Control Loop) directory

--- Python/Assignment-2(Control Loop)/8.py
+++
@@ -1,4 +0,0 @@
-# Q8. Find the absolute value of a number entered through the keyboard.
-number = float(input("Enter a number: "))
-absolute_value = abs(number)
-print(f"The absolute value of the number is: {absolute_value}")
