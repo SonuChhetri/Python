26e78dca5102999aebd1d58f19e3788090d971ba
Sonu Chhetri <sonuchh21@gmail.com>
2024-12-15 10:03:20 +0530

    Update FilterDict.py

--- Python/Assignment-8(Dictionary)/FilterDict.py
+++ Python/Assignment-8(Dictionary)/FilterDict.py
@@ -1,5 +1,5 @@
-''' 13. Write a Python program to filter a dictionary based on values.
-original_dict = {'Cierra Vega': 175, 'Alden Cantrell': 180, 'Kierra Gentry': 165, 'Pierre Cox': 190}'''
+# 13. Write a Python program to filter a dictionary based on values.
+original_dict = {'Cierra Vega': 175, 'Alden Cantrell': 180, 'Kierra Gentry': 165, 'Pierre Cox': 190}
 
 # Filter out people with marks greater than 170
 filtered_dict = {key: value for key, value in original_dict.items() if value > 170}
